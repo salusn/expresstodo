523286a3d145bff6e68a8bdf04bb0dc1996d53fb
salusn <salu.s@lightrains.com>
2017-11-29 11:42:47 +0530

    inserted task into the databse

--- routes/todo.js
+++ routes/todo.js
@@ -1,12 +1,22 @@
 // jshint ignore: start
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://localhost:27017/todo';
 
 router.get('/todo', function(req, res, next) {
   res.render('todo', { title: 'todo' });
 });
 
 router.post('/todo', function(req, res) {
+	MongoClient.connect(url, function(err, db) {
+		var collection = db.collection('todotasks');
+		var todotask = req.body.todotask;		
+		collection.insert({todotask: todotask}, function(err, result) {
+			console.log(result)			    	
+		})
+	});
+	res.redirect('todo');	 
 	
 });
 
